e0ff93bbc86bd9ed6196c028a351a65570c6d5fa
Salak Sen <benismail31@gmail.com>
2018-04-01 18:07:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  for i in 0..9
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts "-"*11
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts "-"*11
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-"*11
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-"*11
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
